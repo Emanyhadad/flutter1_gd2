2db82950643209359c37e680106bd5acd36f4c5e
Emanyhadad <emanhadad17@gmail.com>
2023-09-14 10:42:04 +0300

    test statement

--- lib/FlutterAssignment_1.dart
+++ lib/FlutterAssignment_1.dart
@@ -16,7 +16,7 @@ class FlutterAssignment extends StatelessWidget {
     "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1335&q=80",
     "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1335&q=80",
   ];
-
+// this comit to test git
   @override
   Widget build(BuildContext context) {
     return Scaffold(
